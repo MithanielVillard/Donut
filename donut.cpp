36cfae58d29e1310d92d0b2596736fa67fafe620
MithanielVillard <mvillardhernandez@gaming.tech>
2025-11-04 09:00:24 +0100

    Question 1.3

--- donut.cpp
+++ donut.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#define HOME_ESCAPE "\x1B[H"
+#define CLEAR_ESCAPE "\x1B[2J"
+#define HIDE_ESCAPE "\x1B[?25l"
+#define SHOW_ESCAPE "\x1B[?25h"
+
 int main()
 {
-	std::cout << "hello world" << std::endl;
+	std::cout << CLEAR_ESCAPE << HOME_ESCAPE  << HIDE_ESCAPE << "hello world" << std::endl;
+	std::cout << SHOW_ESCAPE;
 }
